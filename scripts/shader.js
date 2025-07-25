0ce9f987bb47d82049e08571f4bf21e701ded29b
kqyhappy <cylu@seu.edu.cn>
2025-07-25 17:26:09 +0800

    configure phone layout

--- scripts/shader.js
+++ scripts/shader.js
@@ -2,7 +2,6 @@ let scrolled = false;
 window.addEventListener('wheel', function (e) {
 const hero = document.querySelector('.hero-area');
 
-    // 向下滚动并且只触发一次
 if (e.deltaY > 0 && !scrolled) {
       hero.classList.add('scrolled');
       scrolled = true;
